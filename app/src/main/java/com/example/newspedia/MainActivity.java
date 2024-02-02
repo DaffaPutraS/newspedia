358a17cbd2b500d2f2c408cab5046aafbd7c0905
DaffaPutraS <daffap434@gmail.com>
2024-02-02 17:03:04 +0700

    add fragment

--- app/src/main/java/com/example/newspedia/MainActivity.java
+++ app/src/main/java/com/example/newspedia/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.newspedia;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -9,58 +10,69 @@ import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 
 import com.example.newspedia.databinding.ActivityMainBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
-    ActivityMainBinding binding;
-
-    // Konstanta untuk nilai-nilai ID menu
-    private static final int HOME_MENU_ID = R.id.home;
-    private static final int SEARCH_MENU_ID = R.id.search;
-    private static final int BOOKMARK_MENU_ID = R.id.bookmark;
-    private static final int PROFILE_MENU_ID = R.id.profile;
+    private BottomNavigationView bottomNavigationView;
+    private FrameLayout frameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_main);
 
-        // mengganti fragment yang ditampilkan dengan HomeFragment
-        replaceFragment(new HomeFragment());
+        bottomNavigationView = findViewById(R.id.bottomNavView);
+        frameLayout = findViewById(R.id.frame_layout);
 
-        // mengatur orientasi layar menjadi portrait
-        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
-        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
-            int selectedMenuId = item.getItemId();
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
-            if (selectedMenuId == R.id.home) {
-                replaceFragment(new HomeFragment());
-            } else if (selectedMenuId == R.id.search) {
-                replaceFragment(new SearchFragment());
-            } else if (selectedMenuId == R.id.bookmark) {
-                replaceFragment(new BookmarkFragment());
-            } else if (selectedMenuId == R.id.profile) {
-                replaceFragment(new ProfileFragment());
+                int itemId = item.getItemId();
+
+                if (itemId == R.id.home) {
+                    loadFragment(new HomeFragment(), false);
+
+                } else if (itemId == R.id.search) {
+                    loadFragment(new SearchFragment(), false);
+
+                } else if (itemId == R.id.bookmark) {
+                    loadFragment(new BookmarkFragment(), false);
+
+                } else {
+                    loadFragment(new ProfileFragment(), false);
+
+                }
+
+                return true;
             }
-
-            return true;
         });
+
+        loadFragment(new HomeFragment(), true);
+
     }
 
-    // method untuk mengganti fragment yang ditampilkan
-    private void replaceFragment(Fragment fragment) {
-        Log.d("FragmentSwitch", "Replacing fragment with: " + fragment.getClass().getSimpleName());
 
-        // mengambil FragmentManager dari activity
+    private void loadFragment(Fragment fragment, boolean isAppInitialized) {
+
         FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction(); // memulai transaksi fragment
-        fragmentTransaction.replace(R.id.frame_layout, fragment); // mengganti fragment yang ditampilkan pada frame_layout dengan fragment yang di-passing
-        fragmentTransaction.commit(); // menyelesaikan transaksi fragment
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        if (isAppInitialized){
+            fragmentTransaction.add(R.id.frame_layout, fragment);
+        } else {
+            fragmentTransaction.replace(R.id.frame_layout, fragment);
+        }
+
+
+        fragmentTransaction.commit();
     }
 
 }
